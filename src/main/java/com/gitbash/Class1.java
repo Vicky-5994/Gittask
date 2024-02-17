d3ef0825cfb9471d6c90974ffaca1711b4d1c76d
Vicky-5994 <vigneshd5994@gmail.com>
2024-02-17 22:12:08 +0530

    Git automation

---
+++ src/main/java/com/gitbash/Class1.java
@@ -0,0 +1,12 @@
+package com.gitbash;
+
+public class Class1 {
+	public void empname() {
+		System.out.println("vignesh");
+	}
+		public void empid() {
+			System.out.println("1234");
+		}
+	}
+
+
---
+++ src/main/java/com/gitbash/Class2.java
@@ -0,0 +1,11 @@
+package com.gitbash;
+
+public class Class2 {
+	public void cmpname() {
+		System.out.println("precitech");
+	}
+	public void cmpid() {
+		System.out.println("6789");
+	}
+
+}
